9069a34d5c795bb270c5485f521707a8e9580ec5
Ashly Nayara Lavado Galarza <134320640+nanisandkoo@users.noreply.github.com>
2023-07-24 15:52:22 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/Categoria.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nombre;
+
+    @OneToMany(targetEntity = Instrumento.class, mappedBy = "categoria")
+    @OrderBy("nombre ASC")
+    private Set<Instrumento> instrumentos = new HashSet<Instrumento>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Instrumento> getInstrumento(){
+        return instrumentos;
+    }
+    public void setIntrumento(Set<Instrumento> instrumentos){
+        this.instrumentos = instrumentos;
+    }
+    
+}
